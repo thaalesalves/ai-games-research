6741cfa802cff4d632dad795f8cda41012750c06
Thales Alves <thales@thalesalv.es>
2021-02-11 18:31:33 -0300

    improved systems: code cleaned up and reactions based on race added

--- inventory/inputModifier.js
+++ inventory/inputModifier.js
@@ -3,30 +3,44 @@ const modifier = (text) => {
   let modifiedText = text;
   const lowered = text.toLowerCase();
 
-  if (typeof state.inventory == 'undefined') {
-    state.inventory = [];
+  if (!state.init && info.actionCount < 1) {
+    grabAllBrackets(modifiedText);
+    state.character = {
+      name: state.placeholders[0],
+      gender: state.placeholders[1],
+      race: state.placeholders[2],
+      class: 'Mage'
+    };
+
+    const dataBasedOnRace = parseRace(state.character.race);
+    getInventory();
+    state.init = true;
+    modifiedText = modifiedText.replace(BRACKETS, '') + dataBasedOnRace;
   }
 
   if (lowered.includes('inventory')) {
     if (lowered.includes('check')) {
       stop = true;
       modifiedText += checkInventory();
+      state.message = modifiedText;
       console.log(getInventory());
     } else if (lowered.includes('add')) {
       stop = true;
       const itemToBeAdded = lowered.split('add').pop().split('to')[0];
-      const itemName = capitalize(itemToBeAdded.replace(/[0-9]/g, '').trim());
-      const itemQuantity = parseInt(itemToBeAdded.replace(/\D/g, '').trim());
+      const itemName = capitalize(itemToBeAdded.replace(LETTER_REGEX, '').trim());
+      const itemQuantity = parseInt(itemToBeAdded.replace(DIGIT_REGEX, '').trim());
 
       modifiedText += addToInventory(itemName, itemQuantity);
+      state.message = modifiedText;
       console.log(getInventory());
     } else if (lowered.includes('remove')) {
       stop = true;
       const itemToBeRemoved = lowered.split('remove').pop().split('from')[0];
-      const itemName = capitalize(itemToBeRemoved.replace(/[0-9]/g, '').trim());
-      const itemQuantity = parseInt(itemToBeRemoved.replace(/\D/g, '').trim());
+      const itemName = capitalize(itemToBeRemoved.replace(LETTER_REGEX, '').trim());
+      const itemQuantity = parseInt(itemToBeRemoved.replace(DIGIT_REGEX, '').trim());
 
       modifiedText += removeFromInventory(itemName, itemQuantity);
+      state.message = modifiedText;
       console.log(getInventory());
     }
   }
--- inventory/shared.js
+++ inventory/shared.js
@@ -1,39 +1,103 @@
+const LETTER_REGEX = /[0-9]/g;
+const DIGIT_REGEX = /\D/g;
+const BRACKETED = /\[(.*?)\]/g;
+const BRACKETS = /\[|\]/g;
+
+/**
+ * Bracker handler by Gnurro.
+ * 
+ * Removes backets from input text to handle them as placeholders
+ */
+const grabAllBrackets = (text) => {
+  for (entry of text.match(BRACKETED)) {
+    entry = entry.replace(BRACKETS, '');
+    if (!state.placeholders) {
+      state.placeholders = new Array();
+    }
+
+    state.placeholders.push(entry);
+  }
+
+  console.log(state.placeholders);
+}
+
+/**
+ * Function that parses the character's race when they're created
+ * 
+ * @param {string} race
+ */
+const parseRace = (race) => {
+  let returnText = ' ';
+  if (race.toLowerCase().includes("altmer") || race.toLowerCase().includes("high elf")) {
+    addToInventory('Apprentice Mage Robes', 1);
+    addToInventory('Apprentice Mage Boots', 1);
+    addToInventory('Apprentice Mage Hood', 1);
+
+    possibleLines = [
+      '\"What do you want here, elf? Here to enslave us simple Nord folks and make us kneel to your Dominion?\"\n',
+      '\"Another elf? Your kind is not welcome here. Skyrim belongs to the Nords!\"\n',
+      '\"Haran, hide Eirid. There\'s an elf coming in.\"\n',
+      '\"You may stay, but I\'m watchng you, damn elf. One false movement and I\'ll have you thrown in the Sea of Ghosts!\"\n',
+    ]
+
+    returnText += possibleLines[Math.floor(Math.random() * possibleLines.length)];
+  }
+
+  return returnText;
+}
+
+/**
+ * Simple frunction to capitalize the first letter of a string
+ * 
+ * @param {string} string 
+ */
 const capitalize = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+/**
+ * Finds an item in the player's inventory
+ * @param {string} itemName 
+ */
 const findItemInInventory = (itemName) => {
   return getInventory().find(item => {
     return item.name == itemName;
   });
 }
 
+/**
+ * Removes an item from player's inventory
+ * 
+ * @param {string} itemName 
+ * @param {integer} itemQuantity 
+ */
 const removeFromInventory = (itemName, itemQuantity) => {
   let item = findItemInInventory(itemName);
   if (!(item.quantity == itemQuantity) && (item.quantity > 1 && item.quantity >= itemQuantity)) {
     item.quantity -= itemQuantity;
     return `\nYou have removed ${itemQuantity} ${itemName} from your inventory.`;
   }
-  
+
   let index = getInventory().indexOf(item);
   getInventory().splice(index, 1);
   return `\nYou have removed all ${itemName} from your inventory.`;
 }
 
+/**
+ * Checks player's inventory and returns what's inside
+ */
 const checkInventory = () => {
-  let items = '';
 
   if (getInventory().length > 0) {
-    getInventory().forEach((item) => {
-      items += item.quantity + ' ' + item.name + ', ';
-    });
-
-    return `\nYour inventory contains: ${items}`;
+    return `\nYour inventory contains: ` + getInventory().join(', ');
   }
 
   return `\nYour inventory is empty.`;
 }
 
+/**
+ * Returns player's inventory. If the array in undefined, define it
+ */
 const getInventory = () => {
   if (typeof state.inventory == 'undefined') {
     state.inventory = [];
@@ -42,13 +106,14 @@ const getInventory = () => {
   return state.inventory;
 }
 
+/**
+ * Adds item to player's inventory
+ * @param {string} itemName 
+ * @param {integer} itemQuantity 
+ */
 const addToInventory = (itemName, itemQuantity) => {
-  
-  let item = findItemInInventory(itemName);
-  if (typeof state.inventory == 'undefined') {
-    state.inventory = [];
-  }
 
+  let item = findItemInInventory(itemName);
   if (typeof item == 'undefined') {
     item = {
       name: itemName,
