4cbe362e667dae1f3f017538683b10c0a705e964
Thales Alves <thales@thalesalv.es>
2021-04-19 02:41:07 +0100

    removed obsolete scripts

---
+++ tamriel/cyrodiil/random_chars.js
@@ -0,0 +1,25 @@
+const RANDOM_CHARACTERS = [
+  {
+    name: `Hrissia`,
+    gender: `female`,
+    race: `Imperial`,
+    age: '28',
+    personality: 'mean, cantankerous, violent, cruel, mean, hates almost everyone she meets',
+    class: 'bandit',
+    eyes: {
+      eyeColor: 'blue'
+    },
+    hair: {
+      hairStyle: 'long',
+      hairColor: 'blonde'
+    },
+    appearance: {
+      height: '185',
+      weight: '67',
+      features: 'muscular body, long blonde hair, blue eyes, tall, skinny'
+    },
+    storyStart: `You are at your base with your fellow bandits. Your base is located in the Blackwoods, in the Nibenese Plains, just east of the city of Leyawiin. You're relaxing drinking a mug of red ale and eating some pork when a fellow bandit, an Orc by the name of Grog gro-Murak, approaches you.\n"Hrissia, I heard some noble suckers from Skingrad are coming down the road on their way to Leyawiin. Let's mug them, come with me.", Grog gro-Murak says.`,
+    worn: `steel armor and steel shield`,
+    weapon: 'steel sword'
+  }
+];
