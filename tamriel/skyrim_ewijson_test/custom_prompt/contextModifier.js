c749cffecd04c55c4c4193bcde820f01c2a62c26
Thales Alves <thales@thalesalv.es>
2021-04-19 01:53:57 +0100

    added custom prompt scripts

---
+++ tamriel/skyrim_ewijson_test/custom_prompt/contextModifier.js
@@ -0,0 +1,130 @@
+state.data = {} // When processing context, rebuild data. Input modifier interacts with "cached" version.
+dataStorage = state.data;
+delete state.message
+let contextMemory = getMemory(text);
+let context = getContext(text);
+let lines = context.split('\n');
+const linesLength = context.length;
+let memoryLines = contextMemory.split('\n');
+
+let modifiedText = text.toLowerCase();
+let modifiedContext = context.toLowerCase();
+let memoryLinesLength = memoryLines.length
+
+let copyLines = [...lines];
+let copyMemoryLines = [...memoryLines];
+
+const modifier = (text) => {
+  const lowered = text.toLowerCase();
+  let stop = false;
+
+  if (state.shouldStop) {
+    state.shouldStop = false;
+    stop = true;
+  }
+
+  // BEGIN Encounters
+  // for mashing with other context scripts, keep this codeblock before 'const contextMemory'
+  // encounter memory stuff:
+  if (state.encounterMemories) {
+    memoryLoop:
+    for (encounterMemory of state.encounterMemories) {
+      // take care to not overload memory, unless forced:
+      if (!encounterMemory.memoryGreed && (encounterMemory.memoryText.length + memory.length) > 1000) {
+        console.log(`Non-greedy encounterMemory too long, not inserting it!`)
+        continue memoryLoop
+      }
+      state.memory.context = memory
+      if (encounterMemory.memoryLocation == 'bottom') {
+        state.memory.context = `${state.memory.context}\n${encounterMemory.memoryText}`
+      } else if (encounterMemory.memoryLocation == 'top') {
+        state.memory.context = `${encounterMemory.memoryText}\n${state.memory.context}`
+      } else {
+        console.log(`No memoryLocation defined for '${encounterMemory.memoryText}', defaulting to 'top'.`)
+        state.memory.context = `${encounterMemory.memoryText}\n${state.memory.context}`
+      }
+    }
+  }
+  // END Encounters
+
+  // BEGIN EWIJSON
+  const execute = {
+
+    "Sanitize the whitelist.":
+    {
+      "req": true,
+      "args": null,
+      "exec": sanitizeWhitelist
+    },
+    "Build qualified entries as Objects in dataStorage.":
+    {
+      "req": true,
+      "args": null,
+      "exec": buildObjects
+    },
+
+    "Ensure _synonyms is handled first when creating the globalWhitelist.":
+    {
+      "req": Object.keys(dataStorage)[1] != synonymsPath || Object.keys(dataStorage)[0] != whitelistPath,
+      "args": null,
+      "exec": fixOrder
+    },
+    "Build a global whitelist based on context and wildcards.":
+    {
+      "req": true,
+      "args": null,
+      "exec": getGlobalWhitelist
+    },
+    "Sort and execute the Object entries.":
+    {
+      "req": true,
+      "args": null,
+      "exec": insertJSON
+    },
+    "Sort and execute the EWI Attribute entries.":
+    {
+      "req": worldInfo.length > 0,
+      "args": null,
+      "exec": processEWI
+    },
+    "Check the inserted JSON- lines for the presence of worldInfo keywords.":
+    {
+      "req": state.settings["cross"],
+      "args": null,
+      "exec": crossLines
+    },
+    /*  "Add the Stacks":
+     {
+         "req": Stacks,
+         "args": null,
+         "exec": addStacks
+     }, */
+    "Create an always visible entry that displays all created roots for Objects.":
+    {
+      "req": true,
+      "args": null,
+      "exec": trackRoots
+    },
+    "Refresh the variables presented in the HUD.":
+    {
+      "req": state.displayStats,
+      "args": null,
+      "exec": updateHUD
+    }
+
+  }
+
+  for (let action in execute) { if (execute[action]["req"]) { execute[action]["exec"](execute[action]["args"]) } }
+
+  let combinedMemory = memoryLines.join('\n')
+  let combinedLines = lines.join('\n').slice(-(info.maxChars - combinedMemory.length - 1));
+  const finalText = [combinedMemory, combinedLines].join("");
+
+  // Debug to check if the context is intact and properly utilized, optimally the numbers should always match
+  console.log(`Final Text: ${finalText.length}`, `Max Text: ${info.maxChars}`, `MemoryLength: ${info.memoryLength}`, `Total Memory: ${info.memoryLength + contextMemoryLength}`)
+  // END EWIJSON
+
+  return { text: finalText, stop: stop };
+}
+
+modifier(text);
---
+++ tamriel/skyrim_ewijson_test/custom_prompt/outputModifier.js
@@ -0,0 +1,298 @@
+const modifier = (text) => {
+  let modifiedText = text
+  const lowered = text.toLowerCase()
+  // BEGIN EWIJSON
+  if (state.generate.process) { parseGen(text); return { text: '' }; }
+  // END EWIJSON
+  
+  // BEGIN Encounters
+
+  // Debugging action counter: (uncomment to better check global timer-only encounters)
+  // state.displayStats = [{key:'Actions', value: `${info.actionCount}`}]
+
+  // encounter trigger processing
+  if (!state.currentEncounter) {
+    globalLoop:
+    for (encounter in encounterDB) { // go through encounters
+      console.log(`Global checking '${encounter}'...`)
+      /*
+      if (encounterDB[encounter].inputLock) {
+          console.log("Input checking disabled on this encounter.")
+          continue globalLoop
+      }
+      */
+      //for outputMod:
+      if (encounterDB[encounter].outputLock) {
+        console.log("Output checking disabled on this encounter.")
+        continue globalLoop
+      }
+
+      // limiting encounter recurrence:
+      if (state.limitedEncounters) {
+        limitLoop:
+        for (limiter of state.limitedEncounters) {
+          if (limiter[0] == encounter) {
+            console.log(`'${encounter}' recurrence has an active limit.`)
+            if (limiter[1] > 0) {
+              console.log(`'${limiter[0]}' can still happen ${limiter[1]} times.`)
+              break limitLoop
+            } else {
+              console.log(`'${limiter[0]}' can't happen anymore.`)
+              continue globalLoop
+            }
+          }
+        }
+      }
+      if (typeof (state.cooldownEncounters) !== 'undefined') {
+        cooldownLoop:
+        for (cooldown of state.cooldownEncounters) {
+          if (cooldown[0] == encounter) {
+            console.log(`'${encounter}' has an active cooldown.`)
+            continue globalLoop
+          }
+        }
+      }
+      if (typeof (encounterDB[encounter].globalActionDelay) == 'undefined') {
+        console.log(`No global delay on '${encounterDB[encounter].encounterID}'!`)
+        globalActionDelay = 0
+      } else {
+        globalActionDelay = encounterDB[encounter].globalActionDelay
+      }
+      if (info.actionCount < globalActionDelay) {
+        console.log(`It's too early for '${encounterDB[encounter].encounterID}'.`)
+        continue globalLoop
+      }
+      console.log(`Hit more then ${globalActionDelay} total actions, allowing '${encounter}'!`)
+      if (encounterDB[encounter].triggers) {
+        console.log(`'${encounterDB[encounter].encounterID}' has triggers!`)
+        triggerLoop:
+        for (triggerStr of encounterDB[encounter].triggers) {
+          // console.log(triggerStr)
+          triggerRegEx = new RegExp(triggerStr, "gi")
+          caughtTrigger = text.match(triggerRegEx)
+          if (caughtTrigger) {
+            console.log(`Caught '${caughtTrigger}', checking '${encounter}' chance...`)
+            if (!encounterDB[encounter].chance) {
+              console.log(`No chance on triggered '${encounterDB[encounter].encounterID}' detected, this is probably an error!`)
+            } else {
+              console.log(`${encounterDB[encounter].chance}% chance detected!`)
+              if (getRndInteger(1, 100) <= encounterDB[encounter].chance) {
+                console.log(`Rolled below ${encounterDB[encounter].chance} chance, running '${encounter}'!`)
+                updateCurrentEncounter(encounter)
+                break globalLoop
+              } else {
+                console.log(`Rolled above ${encounterDB[encounter].chance} chance, so no '${encounter}'!`)
+              }
+            }
+          }
+        }
+        console.log(`None of the triggers of '${encounterDB[encounter].encounterID}' detected in (text), moving on.`)
+      } else {
+        console.log(`No triggers for '${encounter}' found, check chance...`)
+        if (encounterDB[encounter].chance) {
+          console.log(`${encounterDB[encounter].chance}% chance for '${encounter}' detected!`)
+          if (getRndInteger(1, 100) <= encounterDB[encounter].chance) {
+            console.log(`Rolled below ${encounterDB[encounter].chance} chance, running '${encounter}'!`)
+            updateCurrentEncounter(encounter)
+            break globalLoop
+          } else {
+            console.log(`Rolled above ${encounterDB[encounter].chance} chance, so no '${encounter}'!`)
+          }
+        } else {
+          console.log(`No chance on '${encounterDB[encounter].encounterID}' detected, so it's probably a chain-only encounter!`)
+          continue globalLoop
+        }
+      }
+    }
+  }
+
+  // current encounter processing:
+  if (state.currentEncounter) {
+    if (state.currentEncounter.activationDelay) {
+      console.log(`Delaying by ${state.currentEncounter.activationDelay} actions before running '${state.currentEncounter.encounterID}'!`)
+      state.currentEncounter.activationDelay -= 1
+    } else {
+      console.log(`No delay, running '${state.currentEncounter.encounterID}'!`)
+      // activating encounters:
+      updateCurrentEffects()
+      if (!state.currentEncounter.memoryAdded && state.currentEncounter.memoryAdd) {
+        if (!state.encounterMemories) {
+          state.encounterMemories = []
+        }
+        state.encounterMemories.push(state.currentEncounter.memoryAdd)
+        state.currentEncounter.memoryAdded = true
+      }
+
+      if (!state.currentEncounter.textInserted && state.currentEncounter.textNotes) {
+        curTextNote = getRndFromList(state.currentEncounter.textNotes)
+        // random wordlist inserts:
+        if (typeof (curTextNote) !== 'undefined') {
+          curTextNote = fillPlaceholders(curTextNote)
+          // for outputs:
+          modifiedText += ` ${curTextNote}`
+          // modifiedText += `\n${curTextNote}`
+          state.currentEncounter.textInserted = true
+        }
+      }
+
+      if (!state.currentEncounter.WIadded && state.currentEncounter.addWI) {
+        for (WIentry in state.currentEncounter.addWI) {
+          console.log(`Adding '${state.currentEncounter.addWI[WIentry].keys}' WI entry.`)
+          addWorldEntry(state.currentEncounter.addWI[WIentry].keys, state.currentEncounter.addWI[WIentry].entry, state.currentEncounter.addWI[WIentry].hidden)
+        }
+        state.currentEncounter.WIadded = true
+      }
+
+      // branching encounters:
+      // for outputMod:
+      if (state.currentEncounter.branches && !state.currentEncounter.outputLock) {
+        // if (state.currentEncounter.branches && !state.currentEncounter.inputLock) {
+        branchLoop:
+        for (chkBranch of state.currentEncounter.branches) {
+          console.log(`Checking '${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}'...`)
+
+          if (!chkBranch.branchChance) {
+            console.log(`'${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}' has no chance, this is most likely an error!`)
+            continue branchLoop
+          }
+
+          if (chkBranch.branchTriggers) {
+            console.log(`'${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}' has triggers!`)
+            branchTriggerLoop:
+            for (triggerStr of chkBranch.branchTriggers) {
+              triggerRegEx = new RegExp(triggerStr, "gi")
+              caughtTrigger = text.match(triggerRegEx)
+              if (caughtTrigger) {
+                console.log(`Caught trigger '${caughtTrigger}' for '${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}', checking chance...`)
+                if (getRndInteger(1, 100) <= chkBranch.branchChance) {
+                  console.log(`Rolled below ${chkBranch.branchChance} chance for '${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}', branching!`)
+
+                  if (chkBranch.branchTextNotes) {
+                    curTextNote = getRndFromList(chkBranch.branchTextNotes)
+                    // random wordlist inserts:
+                    if (typeof (curTextNote) !== 'undefined') {
+                      curTextNote = fillPlaceholders(curTextNote)
+                      // for outputs:
+                      modifiedText += ` ${curTextNote}`
+                      // modifiedText += `\n${curTextNote}`
+                      // state.currentEncounter.textInserted = true
+                    }
+                  }
+
+                  if (chkBranch.branchChained) {
+                    updateCurrentEncounter(getRndFromList(chkBranch.branchChained))
+                    break branchLoop
+                  } else {
+                    console.log(`'${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}' has no chained encounter, but this might be intentional.`)
+                  }
+                }
+              }
+            }
+          } else {
+            console.log(`'${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}' has no triggers, using pure chance!`)
+            if (getRndInteger(1, 100) <= chkBranch.branchChance) {
+              console.log(`Rolled below ${chkBranch.branchChance} chance for '${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}', branching!`)
+              if (chkBranch.branchTextNotes) {
+                modifiedText += ` ${getRndFromList(chkBranch.branchTextNotes)}`
+              }
+              if (chkBranch.branchChained) {
+                updateCurrentEncounter(getRndFromList(chkBranch.branchChained))
+                break branchLoop
+              } else {
+                console.log(`'${state.currentEncounter.encounterID}' branch '${chkBranch.branchID}' has no chained encounter, but this might be intentional.`)
+              }
+            }
+          }
+        }
+      }
+
+      // ending encounters:
+      if (typeof (state.currentEncounter) == 'undefined') {
+        console.log(`state.currentEncounter doesn't exist! This can happen due to branching.`)
+      } else {
+        if (state.currentEncounter.endTriggers) {
+          console.log(`${state.currentEncounter.encounterID} has end triggers!`)
+          for (triggerStr of state.currentEncounter.endTriggers) {
+            triggerRegEx = new RegExp(triggerStr, "gi")
+            caughtTrigger = text.match(triggerRegEx)
+            if (caughtTrigger) {
+              console.log(`Caught ${caughtTrigger}, ending '${state.currentEncounter.encounterID}'!`)
+              if (state.currentEncounter.chained) {
+                console.log(`Detected chained encounter(s) on ${state.currentEncounter.encounterID}!`)
+                delete state.message
+                delete state.encounterNote
+                updateCurrentEncounter(getRndFromList(state.currentEncounter.chained))
+              } else {
+                updateCurrentEncounter()
+                updateCurrentEffects()
+              }
+            }
+          }
+        }
+
+        if (typeof (state.currentEncounter) !== 'undefined') {
+          if (typeof (state.currentEncounter.duration) !== 'undefined') {
+            if (state.currentEncounter.duration > 0) {
+              console.log(`Keeping up ${state.currentEncounter.encounterID} for ${state.currentEncounter.duration} more actions!`)
+              state.currentEncounter.duration -= 1
+            } else {
+              console.log(`Duration of ${state.currentEncounter.encounterID} over!`)
+              if (state.currentEncounter.chained) {
+                console.log(`Detected chained encounter(s) on ${state.currentEncounter.encounterID}!`)
+                delete state.message
+                delete state.encounterNote
+                updateCurrentEncounter(getRndFromList(state.currentEncounter.chained))
+              } else {
+                updateCurrentEncounter()
+                updateCurrentEffects()
+              }
+            }
+          } else {
+            console.log(`No duration on ${state.currentEncounter.encounterID}, keeping it up infinitely!`)
+          }
+        }
+      }
+    }
+  }
+
+
+  // encounter memory stuff:
+  if (state.encounterMemories) {
+    for (encounterMemory of state.encounterMemories) {
+      if (encounterMemory.memoryLingerDuration >= 1) {
+        console.log(`'${encounterMemory.memoryText}' will stay in memory for ${encounterMemory.memoryLingerDuration} more actions.`)
+        encounterMemory.memoryLingerDuration -= 1
+      } else {
+        console.log(`'${encounterMemory.memoryText}' will no longer stay in memory.`)
+        state.encounterMemories.splice(state.encounterMemories.indexOf(encounterMemory), 1)
+      }
+    }
+  }
+
+  if (state.cooldownEncounters) {
+    console.log(`Cooldowns detected!`)
+    cooldownLoop:
+    for (cooldown in state.cooldownEncounters) {
+      if (state.cooldownEncounters[cooldown] == null) { // safety/legacy...
+        state.cooldownEncounters.splice(cooldown, 1)
+        continue cooldownLoop
+      }
+      console.log(`'${state.cooldownEncounters[cooldown][0]}' (${cooldown}) cooldown: ${state.cooldownEncounters[cooldown][1]}.`)
+      state.cooldownEncounters[cooldown][1] -= 1
+      if (state.cooldownEncounters[cooldown][1] <= 0) {
+        console.log(`${state.cooldownEncounters[cooldown][0]} cooldown over, removing.`)
+        state.cooldownEncounters.splice(cooldown, 1)
+      }
+    }
+    if (state.cooldownEncounters[0] == null) {
+      console.log(`No more cooldowns, removing array.`)
+      delete state.cooldownEncounters
+    }
+  }
+  // END Encounters
+
+  return { text: modifiedText }
+}
+
+// Don't modify this part
+modifier(text)
