45675705e30168298efc5fe79847120cf98aa268
Thales Alves <thales@thalesalv.es>
2021-04-19 03:53:26 +0100

    cyrodiil random characters

--- tamriel/cyrodiil/random_chars.js
+++ tamriel/cyrodiil/random_chars.js
@@ -4,7 +4,7 @@ const RANDOM_CHARACTERS = [
     gender: `female`,
     race: `Imperial`,
     age: '28',
-    personality: 'mean, cantankerous, violent, cruel, mean, hates almost everyone she meets',
+    personality: 'mean,violent,cruel',
     class: 'bandit',
     eyes: {
       eyeColor: 'blue'
@@ -16,7 +16,7 @@ const RANDOM_CHARACTERS = [
     appearance: {
       height: '185',
       weight: '67',
-      features: 'muscular body, long blonde hair, blue eyes, tall, skinny'
+      features: 'muscular,tall,skinny'
     },
     storyStart: `You are at your base with your fellow bandits. Your base is located in the Blackwoods, in the Nibenese Plains, just east of the city of Leyawiin. You're relaxing drinking a mug of red ale and eating some pork when a fellow bandit, an Orc by the name of Grog gro-Murak, approaches you.\n"Hrissia, I heard some noble suckers from Skingrad are coming down the road on their way to Leyawiin. Let's mug them, come with me.", Grog gro-Murak says.`,
     worn: `steel armor and steel shield`,
